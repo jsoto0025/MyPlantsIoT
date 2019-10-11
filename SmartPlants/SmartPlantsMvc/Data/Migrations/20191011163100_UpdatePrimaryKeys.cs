61d6f489fc7a13e79ee8e5bc9b4a9e5c16298b74
jsoto25@hotmail.com <jsoto25@hotmail.com>
2019-10-11 12:56:09 -0500

    Actualizacion de las migraciones

---
+++ SmartPlants/SmartPlantsMvc/Data/Migrations/20191011163100_UpdatePrimaryKeys.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartPlantsMvc.Data.Migrations
+{
+    public partial class UpdatePrimaryKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "PlantTypes",
+                newName: "PlantTypeId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Modules",
+                newName: "ModuleId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Farms",
+                newName: "FarmId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlantTypeId",
+                table: "PlantTypes",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ModuleId",
+                table: "Modules",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "FarmId",
+                table: "Farms",
+                newName: "Id");
+        }
+    }
+}
