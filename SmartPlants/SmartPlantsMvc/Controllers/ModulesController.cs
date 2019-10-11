4e5d4f29ca64d75698a053e8e2ebbd9942942112
jsoto25@hotmail.com <jsoto25@hotmail.com>
2019-10-11 12:51:48 -0500

    Modulo de controladores

---
+++ SmartPlants/SmartPlantsMvc/Controllers/ModulesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SmartPlantsMvc.Data;
+using SmartPlantsMvc.Models;
+
+namespace SmartPlantsMvc.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModulesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modules
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Modules.Include(t => t.Farm).Include(t => t.PlantType);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Modules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(t => t.Farm)
+                .Include(t => t.PlantType)
+                .FirstOrDefaultAsync(m => m.ModuleId == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // GET: Modules/Create
+        public IActionResult Create()
+        {
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "Name");
+            ViewData["PlantTypeId"] = new SelectList(_context.PlantTypes, "PlantTypeId", "Name");
+            return View();
+        }
+
+        // POST: Modules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ModuleId,FarmId,PlantTypeId")] Module @module)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@module);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "FarmId", @module.FarmId);
+            ViewData["PlantTypeId"] = new SelectList(_context.PlantTypes, "PlantTypeId", "PlantTypeId", @module.PlantTypeId);
+            return View(@module);
+        }
+
+        // GET: Modules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules.FindAsync(id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "Name", @module.FarmId);
+            ViewData["PlantTypeId"] = new SelectList(_context.PlantTypes, "PlantTypeId", "Name", @module.PlantTypeId);
+            return View(@module);
+        }
+
+        // POST: Modules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ModuleId,FarmId,PlantTypeId")] Module @module)
+        {
+            if (id != @module.ModuleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@module);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModuleExists(@module.ModuleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "Name", @module.FarmId);
+            ViewData["PlantTypeId"] = new SelectList(_context.PlantTypes, "PlantTypeId", "Name", @module.PlantTypeId);
+            return View(@module);
+        }
+
+        // GET: Modules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(t => t.Farm)
+                .Include(t => t.PlantType)
+                .FirstOrDefaultAsync(m => m.ModuleId == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // POST: Modules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @module = await _context.Modules.FindAsync(id);
+            _context.Modules.Remove(@module);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModuleExists(int id)
+        {
+            return _context.Modules.Any(e => e.ModuleId == id);
+        }
+    }
+}
