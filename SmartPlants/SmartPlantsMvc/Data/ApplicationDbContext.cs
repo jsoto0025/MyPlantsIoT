71650a3f154e722c7679325c9814f3c811e0a3d1
jsoto25@hotmail.com <jsoto25@hotmail.com>
2019-10-11 07:11:30 -0500

    Migraciones de base de datos iniciales

---
+++ SmartPlants/SmartPlantsMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartPlantsMvc.Models;
+
+namespace SmartPlantsMvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Farm> Farms { get; set; }
+
+        public DbSet<PlantType> PlantTypes { get; set; }
+
+        public DbSet<Module> Modules { get; set; }
+    }
+}
---
+++ SmartPlants/SmartPlantsMvc/Data/Migrations/20191011115729_AddTableModules02.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartPlantsMvc.Data.Migrations
+{
+    public partial class AddTableModules02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FarmId = table.Column<int>(nullable: false),
+                    PlantTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modules_Farms_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Modules_PlantTypes_PlantTypeId",
+                        column: x => x.PlantTypeId,
+                        principalTable: "PlantTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_FarmId",
+                table: "Modules",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_PlantTypeId",
+                table: "Modules",
+                column: "PlantTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Modules");
+        }
+    }
+}
